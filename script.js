3372f9cb3988f7a7f69290407028c12c73e47ac8
gonzagaa <iigust4vo@gmail.com>
2024-10-07 16:57:08 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//Preloader ==============
+let elem_preloader = document.getElementById("preloader");
+let body = document.getElementById("body");
+
+body.classList.add('no-scroll');
+
+  setTimeout(function() {
+    elem_preloader.classList.remove("preloader");
+    body.classList.remove("no-scroll");
+  }, 5280);
+
 //menu navigation ==============
 window.addEventListener('scroll', onScroll)
 
